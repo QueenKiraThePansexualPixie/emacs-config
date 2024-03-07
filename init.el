f93267f30856924d57eab8782fa6f3fd320c4d35
Frank Hudson <frankoflinux@gmail.com>
2024-03-07 22:07:06 +0000

    Add Golang dependencies

--- init.el
+++ init.el
@@ -16,7 +16,7 @@
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
  '(package-selected-packages
-   '(atom-one-dark-theme multiple-cursors company flycheck lsp-ui lsp-mode tree-sitter-langs tree-sitter rust-mode)))
+   '(go-mode flycheck-ocaml lsp-scheme lsp-latex lsp-java lsp-haskell lsp-docker company-lua company-anaconda atom-one-dark-theme multiple-cursors company flycheck lsp-ui lsp-mode tree-sitter-langs tree-sitter rust-mode)))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
@@ -45,10 +45,15 @@
   (add-hook 'tree-sitter-after-on-hook #'tree-sitter-hl-mode))
 
 (require 'rust-mode)
-(add-hook 'rust-mode-hook
-	  (lambda () (setq indent-tabs-mode nil)))
+(add-hook 'rust-mode-hook (lambda () (setq indent-tabs-mode nil)))
 (add-hook 'rust-mode-hook #'tree-sitter-mode)
 (add-hook 'rust-mode-hook #'lsp)
+
+(require 'go-mode)
+(add-hook 'go-mode-hook #'lsp-deferred)
+(add-hook 'go-mode-hook #'tree-sitter-mode)
+(add-hook 'go-mode-hook (lambda () (setq indent-tabs-mode nil)))
+(add-hook 'go-mode-hook (lambda () (setq tab-width 4)))
 
 ;; THEMES
 
